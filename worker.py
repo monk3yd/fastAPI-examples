a99baa58cecc37f94606092ecdbf28bc88c9d9ff
monk3yd <jegajardog@gmail.com>
2022-11-13 16:01:39 -0300

    Combine path & query parameters

--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 # Inventory Management System Example
 # https://www.youtube.com/watch?v=-ykeT6kk4bk
@@ -21,8 +22,15 @@ def get_item(item_id: int = Path(None, description="The ID of the item you'd lik
 
 # Query parameter(s) example
 @app.get("/get-by-name")
-def get_name(name: str = None):
+def get_name(uid: int, name: Optional[str] = None):
     for item_id in inventory:
         if inventory[item_id]["name"] == name:
             return inventory[item_id]
+    return {"Data": "Not Found"}
+
+# Path & Query parameter(s) example
+@app.get("/get-by-brand/{item_id}")
+def get_brand(item_id: int, name: Optional[str] = None):
+    if inventory[item_id]["name"] == name:
+        return inventory[item_id]
     return {"Data": "Not Found"}
