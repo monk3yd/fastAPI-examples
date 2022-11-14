19d818b25514c5d9cb64ceb1e6dac7f9407d209c
monk3yd <jegajardog@gmail.com>
2022-11-13 22:02:45 -0300

    Request body

--- worker.py
+++ worker.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydanctic import BaseModel
 
 # Inventory Management System Example
 # https://www.youtube.com/watch?v=-ykeT6kk4bk
 # https://fastapi.tiangolo.com/
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
 
 inventory = {
     1: {
@@ -16,12 +23,12 @@ inventory = {
 }
 
 # Path parameter(s) example
-@app.get("/get-item/{item_id}")
+@app.get("/get-item/{item_id}")  # Endpoint
 def get_item(item_id: int = Path(None, description="The ID of the item you'd like to view.")):
     return inventory[item_id]
 
 # Query parameter(s) example
-@app.get("/get-by-name")
+@app.get("/get-by-name")  # Endpoint
 def get_name(uid: int, name: Optional[str] = None):
     for item_id in inventory:
         if inventory[item_id]["name"] == name:
@@ -29,8 +36,14 @@ def get_name(uid: int, name: Optional[str] = None):
     return {"Data": "Not Found"}
 
 # Path & Query parameter(s) example
-@app.get("/get-by-brand/{item_id}")
+@app.get("/get-by-brand/{item_id}")  # Endpoint
 def get_brand(item_id: int, name: Optional[str] = None):
     if inventory[item_id]["name"] == name:
         return inventory[item_id]
     return {"Data": "Not Found"}
+
+
+# Request body parameter(s) example
+@app.post("/create-item/{item_id}")  # Endpoint
+def create_item(item_id: int, item: Item):
+    return {}
